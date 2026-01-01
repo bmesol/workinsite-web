fd3ecb024f5369ad5d737ac0aa97d04bf85f2353
Sengottaiyan P <sengottaiyan@bmesolutions.in>
2026-01-01 13:44:13 +0530

    Test commit for GitHub pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "./shared/components/ui/spinner";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center gap-4 bg-[rgb(245,245,245)]">
+    <div className="w-screen h-screen flex items-center justify-center gap-4 bg-[rgb(245,245,245)] text-red-500">
       Comming soon <Spinner className="size-8" />
     </div>
   );
